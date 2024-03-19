cb0a1943606707c310a7d114de155307577c7668
David Arenas Orozco <darozco25@gmail.com>
2024-03-19 14:59:40 -0400

    Página full sport definitiva

---
+++ RepasoPOO/src/Ejercicios/Main.java
@@ -0,0 +1,11 @@
+package Ejercicios;
+public class Main {
+    public static void main(String[] args){
+
+        Persona persona = new Persona();
+        persona.Registrarse();
+        persona.MostrarDatos();
+        persona.EsMayorDeEdad();
+
+    }
+}
